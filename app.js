ffebd12bc2cc0f91e3ea8286b94f268154620860
Loren Heubert-Aubry <lorenha@umich.edu>
2017-01-30 17:15:08 -0500

    improved opening links from site

--- app.js
+++ app.js
@@ -27,4 +27,8 @@ $( document ).ready(function(){
             }
     });
 
+    $('a').on('click touchstart', function(){
+        $(this).closest('.flip-container').removeClass('flip');
+    });
+
 });
