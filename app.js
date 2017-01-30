46123c33a0b8d8e3109a6a029b15f0e726abc4d2
Loren Heubert-Aubry <lorenha@umich.edu>
2017-01-30 14:07:17 -0500

    fixed scrollbar issue

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 $( document ).ready(function(){
 
     $('.col-lg-6, .col-md-6, .col-sm-6, .col-xs-12').on('click touchstart', function(){
-
-        if($('.flip-container').hasClass('flip')){
+        if ($(window).innerWidth() <= 768) {
+            if($('.flip-container').hasClass('flip')){
+                $('body').css({
+                    'overflow-y': 'hidden'
+                });
+            }
+            else{
             $('body').css({
-                'overflow-y': 'hidden'
-            });
-        }
-        else{
-        $('body').css({
-                'overflow-y': 'auto'
-            }); 
+                    'overflow-y': 'auto'
+                }); 
+            }
         }
     });
 
