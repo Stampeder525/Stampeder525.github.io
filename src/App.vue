bb1b344767aacb8b8c6ebe0c6c9cc8060d6ad22d
Loren Heubert-Aubry <lorenha@umich.edu>
2018-08-16 01:45:12 -0400

    and other stuff

--- src/App.vue
+++ src/App.vue
@@ -92,8 +92,6 @@ export default {
   },
   mounted() {
     this.$nextTick(function () {
-      // Code that will run only after the
-      // entire view has been rendered
       this.loading = false;
     })
   }
@@ -101,8 +99,6 @@ export default {
 </script>
 
 <style lang="scss">
-// @import url('https://fonts.googleapis.com/css?family=family=Open+Sans:100');
-// @import url('https://fonts.googleapis.com/css?family=Montserrat');
 
 [v-cloak]{
   display: none !important;
